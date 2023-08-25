ed1037dfdc5d6d3c73bbe26503b387248e42ead0
AsifOni <asif.oni@astralforminc.com>
2023-08-25 10:42:52 -0400

    Added rewards git

--- create-ccg-app.js
+++ create-ccg-app.js
@@ -7,14 +7,14 @@ const fs = require("fs");
 if (process.argv.length < 3) {
   console.log("You have to provide a name to your app.");
   console.log("For example :");
-  console.log("    npx create-ccg-app my-app");
+  console.log("    npx create-ccg-rewards-app my-app");
   process.exit(1);
 }
 
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const git_repo = "https://github.com/AsifOni/ccg-boilerplate.git";
+const git_repo = "https://github.com/AsifOni/ccgx-rewards.git";
 
 try {
   fs.mkdirSync(projectPath);
